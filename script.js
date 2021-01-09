52039030686cdeabfe7e0d813688c8b5226dee82
IBRA <ibrahimmerhi@mail.ru>
2021-01-09 21:13:24 +0800

    change font family

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var color3 = document.querySelector('.color3');
+var font = document.querySelector('.font');
 var body = document.getElementById('gradient');
 var css = document.querySelector('.css');
+var h1 = document.querySelector('h1');
 
 function changeColor() {
     body.style.background = "linear-gradient(to right , " + color1.value + "," + color2.value + ")";
@@ -11,6 +13,11 @@ function changeColor() {
 function changeTextColor() {
     body.style.color = color3.value;
 }
+function changeFont() {
+    body.style.fontFamily = font.options[font.selectedIndex].text;
+    h1.style.fontFamily = font.options[font.selectedIndex].text;
+}
 color1.addEventListener('input', changeColor);
 color2.addEventListener('input', changeColor);
 color3.addEventListener('input', changeTextColor);
+font.addEventListener('change', changeFont);
